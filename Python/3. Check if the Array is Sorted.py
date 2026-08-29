7ecd71c95fb303e413cfe5f07829953bbd2b390f
manika-divya <manikadivya2005@gmail.com>
2026-08-29 21:57:59 +0530

    3. Check if the Array is Sorted

---
+++ Python/3. Check if the Array is Sorted.py
@@ -0,0 +1,13 @@
+arr = list(map(int,input("Enter the values: ").split()))
+
+is_sorted = True
+
+for i in range(1, len(arr)):
+    if arr[i] < arr[i - 1]:
+        is_sorted = False
+        break
+
+if is_sorted:
+    print("Array is sorted")
+else:
+    print("Array is not sorted")
